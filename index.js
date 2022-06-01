98e8a85e0344affee6114370a2b80aa90706f90f
Mohammed Suleiman <mohammed-rideflag@Ferrakkems-MacBook-Pro.local>
2022-06-01 15:20:51 -0400

    support different language

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).toISOString();
 var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString();
 let tripCountObj = {};
 let statsObj = {};
+let lang = "en";
+
+//Languages
+let rules = "";
+let updated = "";
+let expresscarpool = "";
+let tripscompletedTxt = "";
+let tripsRemainingTxt = "";
+let regionRulesTxt = "";
 
 console.log(firstDay)
 console.log(lastDay)
+
 
 window.onload = function() {
     try {
@@ -19,6 +29,10 @@ window.onload = function() {
       region = url.searchParams.get("region");
       userId = url.searchParams.get("user_id");
       env = url.searchParams.get("env");
+      lang = url.searchParams.get("lang");
+      
+      if(lang == null){ lang = "en"; }
+      translate(lang);
       if(userId != null && region){
         getTripCount(firstDay, lastDay, region, userId);
         //getRegionStatistics(region);
@@ -35,9 +49,9 @@ let maxTripsPerday = 0;
 let reward = "";
 
 function drawChart(tripsCount){
-  document.getElementById("rules").innerHTML = "Rules";
-  document.getElementById("updated").innerHTML = "Updated November 19th, 2021";
-  document.getElementById("expresscarpool").innerHTML = "Express Carpool Check";
+  document.getElementById("rules").innerHTML = rules;
+  document.getElementById("updated").innerHTML = updated;
+  document.getElementById("expresscarpool").innerHTML = expresscarpool;
   document.getElementById("mtc").style.display = "none";
     tripsCompleted = tripCountObj.total_count;
     console.log(`tripsCount : ${tripsCount}`);
@@ -59,7 +73,7 @@ function drawChart(tripsCount){
     };
     
     var chDonutData1 = {
-        labels: ['Trips Completed', 'Trips Remaining'],
+        labels: [tripscompletedTxt, tripsRemainingTxt],
         datasets: [
           {
             backgroundColor: colors.slice(0,3),
@@ -126,8 +140,15 @@ function getTripCount(start_time, end_time, region, userId){
       let regionStats = data.Payload;
       statsObj = regionStats;
       console.log(`${JSON.stringify(statsObj)}`);
+      regionRulesTxt = statsObj.rules;
+
+      if(lang == "en") { }
+      else if(lang == "fr") { regionRulesTxt = statsObj.rulesFr; }
+      else if (lang == "es") { regionRulesTxt = statsObj.rulesSp; }
+      else { regionRulesTxt = statsObj.rules; }
+
       document.getElementById('rewardPicture').src = statsObj.reward_image;
-      document.getElementById('rulesOffer').innerHTML = statsObj.rules;
+      document.getElementById('rulesOffer').innerHTML = regionRulesTxt;
       tripsRegionCount = statsObj.tripsCount;
       drawChart(tripsRegionCount);
     });
@@ -141,4 +162,35 @@ function getRegionTripCount(region){
         //default at the moment for other regions
         tripsRegionCount = 10;
     }
+}
+
+
+function translate(lang){
+  if(lang == "en"){
+     rules = "Rules";
+     updated = "Updated November 19th, 2021";
+     expresscarpool = "Express Carpool Check";
+     tripscompletedTxt = "Trips Completed";
+     tripsRemainingTxt = "Trips Remaining";
+
+  }else if(lang == "fr"){
+     rules = "Règles";
+     updated = "Mis à jour le 19 novembre 2021";
+     expresscarpool = "Vérification de covoiturage express";
+     tripscompletedTxt = "Voyages terminés";
+     tripsRemainingTxt = "Voyages restants";
+  }else if(lang == "es"){
+    rules = "Normas";
+    updated = "Actualizado el 19 de noviembre de 2021";
+    expresscarpool = "Cheque de viaje compartido exprés";
+    tripscompletedTxt = "Viajes completados";
+    tripsRemainingTxt = "Viajes restantes";
+  }else{
+    rules = "Rules";
+    updated = "Updated November 19th, 2021";
+    expresscarpool = "Express Carpool Check";
+    tripscompletedTxt = "Trips Completed";
+    tripsRemainingTxt = "Trips Remaining";
+  }
+
 }
